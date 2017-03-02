a3745f6041d17f323c08e45d4c413036f577db2f
Olamilekan <lwahab@terragonltd.com>
2017-03-02 17:20:23 +0100

    Message post test

--- bot/bot.py
+++ bot/bot.py
@@ -1,67 +1,39 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from flask import make_response
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 import os
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
-	def get(self):
-		return {'hello': 'world'}
+    def get(self):
+        return {'hello': 'world'}
+
 
 class WebHook(Resource):
-	def get(self):
-		args = request.args
-		verify_token = 'python_rocks'
+    def get(self):
+        args = request.args
+        verify_token = 'python_rocks'
 
-		if args.get('hub.mode') == 'subscribe' and args.get('hub.verify_token') == verify_token:
-			print('validating webhook')
-			return make_response(args.get('hub.challenge').strip("\n\""))
-		else:
-			print('Failed validation. Make sure the \
-				validation tokens match')
+        if args.get('hub.mode') == 'subscribe' and args.get('hub.verify_token') == verify_token:
+            print('validating webhook')
+            return make_response(args.get('hub.challenge').strip("\n\""))
+        else:
+            print('Failed validation. Make sure the validation token match')
 
-
-	def post(self):
-		args = request.args
-		print args
-
+    def post(self):
+        args = request.args
+        print(jsonify(args))
+        print(request.json)
+        return args
 
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
