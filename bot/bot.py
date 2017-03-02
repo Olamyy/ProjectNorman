e0bcd6f3279f7de68bc342600a758098e6853155
Olamilekan <lwahab@terragonltd.com>
2017-03-02 23:02:11 +0100

    Error fix

--- bot/bot.py
+++ bot/bot.py
@@ -29,20 +29,8 @@ class WebHook(Resource):
         args = request.args
         sender = args['entry'][0]['messaging'][0]['sender']['id']
         message = args['entry'][0]['messaging'][0]['message']['text']
-        self.reply(sender, message[::-1])
+        print(sender, message)
         return "ok"
-
-    def reply(self, user_id, msg):
-        data = {
-            "recipient": {"id": user_id},
-            "message": {"text": msg}
-        }
-        resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="
-                             +
-                             "EAAS0PtgoBk4BAElZCZAVTSSvnIbp22YIcWHTZAvbaSvN5TZCud1unGoFDmOaCr6KZCIH72UUGgUO"
-                             "16XQlj7xXVdg9nBv7j6YqpeQ21m6bGASd7idhMHDZBagymIMggstRiheB3SQxjnPD0t9n7tMP872"
-                             "O6Bikny7Ld4DZBie9e3fgZDZD2", json=data)
-        print(resp.content)
 
 
 api.add_resource(HelloWorld, '/')
