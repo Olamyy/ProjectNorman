b077557027d673cfa947a8f91049dadd91fc6a76
Phvash <davash001@gmail.com>
2017-03-05 16:37:34 +0100

    bot postbot test1

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from flask import make_response
 from flask_restful import Resource, Api, reqparse
 import os
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -20,13 +21,27 @@ class WebHook(Resource):
 			print('validating webhook')
 			return make_response(args.get('hub.challenge').strip("\n\""))
 		else:
-			print('Failed validation. Make sure the \
-				validation tokens match')
+			print('Failed validation. Make sure the validation tokens match')
 
 
 	def post(self):
-		data = request.get_json(force=True)
-		print data
+		message_entries = request.json
+		for entry in message_entries:
+			messagings = entry['messsaging']
+			for message in messagings:
+				sender = message['sender']['id']
+				if message.get('message'):
+					text = message['message']['text']
+					print("{} says {}".format(sender, text))
+				reply(sender, text)
+
+	def reply(user_id, msg):
+		data = {
+		"recipient": {"id": user_id},
+		"message": {"text": msg}
+		}
+		resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+		print(resp.content)
 
 
 
@@ -35,7 +50,7 @@ api.add_resource(WebHook, '/webhook')
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
