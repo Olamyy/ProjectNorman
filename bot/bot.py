e14cd897337c716a01fc46b45631bddd535941d0
Phvash <davash001@gmail.com>
2017-03-05 20:04:42 +0100

    response fixed

--- bot/bot.py
+++ bot/bot.py
@@ -4,6 +4,7 @@ from flask import make_response
 from flask_restful import Resource, Api
 import os
 import json
+import requests as r
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,32 +26,72 @@ class WebHook(Resource):
         	print('Failed validation. Make sure the validation tokens match')
 
     def post(self):
-        message_entries = request.get_json()
-        # print('message_entries1\n', message_entries1)
-        # print(type(request.json))
-        # print(type(request.data))
-        # print(type(request.get_json()))
-        # message_entries = json.loads(request.data)
-        # message_entries = json.loads(request.data.decode('utf8'))['entry']
-        # print('message_entries\n', message_entries1)
-        print(message_entries)
-        for entry in message_entries:
-            messagings = entry['messsaging']
-            for message in messagings:
-                sender = message['sender']['id']
-                if message.get('message'):
-                    text = message['message']['text']
-                    print("{} says {}".format(sender, text))
-                self.reply(sender, text)
+        data = request.get_json()
+        print(data)
+        if data.get('object', None) == 'page':
+            message_entries = data['entry']
+            for entry in message_entries:
+                pageID = entry['id']
+                timeOfEvent = entry['time']
+                for event in entry['messaging']:
+                    if event.get('message', None):
+                        self.reply2(event)
 
-    def reply(self, user_id, msg):
-    	ACCESS_TOKEN = 'EAAS0PtgoBk4BAElZCZAVTSSvnIbp22YIcWHTZAvbaSvN5TZCud1unGoFDmOaCr6KZCIH72UUGgUO16XQlj7xXVdg9nBv7j6YqpeQ21m6bGASd7idhMHDZBagymIMggstRiheB3SQxjnPD0t9n7tMP872O6Bikny7Ld4DZBie9e3fgZDZD2'
-    	data = {
-    	"recipient": {"id": user_id},
-    	"message": {"text": msg}
-    	}
-    	resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    	print(resp.content)
+
+
+        # for entry in message_entries:
+        #     messagings = entry['messsaging']
+        #     for message in messagings:
+        #         sender = message['sender']['id']
+        #         if message.get('message'):
+        #             text = message['message']['text']
+        #             print("{} says {}".format(sender, text))
+        #         self.reply(sender, text)
+
+    def reply2(self, event):
+        senderID = event['sender']['id']
+        recipientID = event['recipient']['id']
+        timeOfMessage = event['timestamp']
+        message = event['message']
+
+        messageId = message['mid']
+        messageText = message.get('text', None)
+        messageAttachments = message.get('attachments', None)
+
+        if messageText:
+            if messageText == 'generic':
+                self.sendGenericMessage(senderID)
+            else:
+                self.sendTextMessage(senderID, messageText)
+        elif messageAttachments:
+            print("message with attachement received")
+            sendTextMessage(senderID)
+
+    def sendGenericMessage(self, recipientID, messageText):
+        pass
+
+
+    def sendTextMessage(self, recipientID, messageText):
+        messageDataDict = {'recipient':{'id': recipientID}, 'message':{'text': messageText}}
+        messageData = json.dumps(messageDataDict)
+        self.callSendAPI(messageData)
+
+    def callSendAPI(self, message):
+        access_token = 'EAAS0PtgoBk4BAAV6pDrocKGlPOAjdxynxBidP5noah1l27yRu2x0zZAc1clvjQN1YAY9dHgJKGd8fPqjwHBxQ0KuQxUahCkxUDmecH9OQvAk8FKuZBZA2jQhHPihh85WcALD8UZBMRxYXT1iAqaAGdpUcjKAlOBwL6VwisVC8QZDZD'
+        uri = 'https://graph.facebook.com/v2.6/me/messages/access_token=' + access_token
+        resp = r.post(uri, json=message)
+        print(resp.status)
+
+
+
+    # def reply(self, user_id, msg):
+    # 	ACCESS_TOKEN = 'EAAS0PtgoBk4BAElZCZAVTSSvnIbp22YIcWHTZAvbaSvN5TZCud1unGoFDmOaCr6KZCIH72UUGgUO16XQlj7xXVdg9nBv7j6YqpeQ21m6bGASd7idhMHDZBagymIMggstRiheB3SQxjnPD0t9n7tMP872O6Bikny7Ld4DZBie9e3fgZDZD2'
+    # 	data = {
+    # 	"recipient": {"id": user_id},
+    # 	"message": {"text": msg}
+    # 	}
+    # 	resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    # 	print(resp.content)
 
 
 api.add_resource(HelloWorld, '/')
